1263d663854e7d8f469ebf91a4ffa11c37fb584c
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-02-27 00:43:35 +1000

    dns pcap: start tcpdump only after all pods have started

--- pcap-dns-pods.sh
+++ pcap-dns-pods.sh
@@ -8,24 +8,31 @@ trap cleanup EXIT
 trap "exit 0" SIGINT
 
 declare -r DNS_PCAP_NS=dns-pcap
+declare  -r LOCAL_PCAP_DIR="dns-pcaps/$(date +%s)"
 
 declare -a TCPDUMP_PIDS=()
 declare -a DEBUG_PODS=()
 declare -a NODE_NAMES=()
-declare  LOCAL_PCAP_DIR="dns-pcaps/$(date +%s)"
 
 log() {
   echo $(date -u +"%Y-%m-%dT%H:%M:%S%Z [%s]") ": $@"
 }
 
+sub_header() {
+  echo -e "\n*  $@"
+  echo "-------------------------------------------------------"
+}
+
+
 CLEANUP_IN_PROGRESS=false
 cleanup() {
-  echo -e "\n-----------------------------------------------------"
+  sub_header "Gathering all pcaps and cleaning up ..."
   $CLEANUP_IN_PROGRESS && return 0
 
   CLEANUP_IN_PROGRESS=true
-  log Shutting down tcpdump and copying pcaps
-  log Please wait until all the pcaps are copied to $LOCAL_PCAP_DIR
+
+  log "Shutting down tcpdump and copying pcaps"
+  log "Please wait until all the pcaps are copied to $LOCAL_PCAP_DIR"
 
   ### DO NOT EXIT on ERRORS
   #set +e
@@ -35,10 +42,13 @@ cleanup() {
   cp_pcaps
   delete_debug_pods
 
-  echo -e "\n==================xxxXxxx=========================="
+
+  sub_header "All pcaps have been copied to $LOCAL_PCAP_DIR"
+  echo -e "\n====================xxxXxxx=========================="
 }
 
 stop_listening() {
+  sub_header  "Stopping tcpdump on all pods"
 
   for pod in ${DEBUG_PODS[@]}; do
     oc exec -t -n $DNS_PCAP_NS $pod  -- pkill tcpdump
@@ -52,12 +62,14 @@ stop_listening() {
 }
 
 cp_pcaps() {
+  sub_header "Copying all pcaps to $LOCAL_PCAP_DIR"
+
   mkdir -p "$LOCAL_PCAP_DIR"
 
   local idx=0
   for pod in ${DEBUG_PODS[@]}; do
     local pcap_file="${NODE_NAMES[$idx]}.pcap"
-    log "copying from $pod -> $pcap_file"
+    log "$pod -> $pcap_file"
 
     #set -x
     oc cp -n $DNS_PCAP_NS $pod:tmp/$pcap_file "$LOCAL_PCAP_DIR/$pcap_file"
@@ -66,17 +78,20 @@ cp_pcaps() {
 }
 
 delete_debug_pods() {
+  sub_header "Deleting all debug pods "
+
   for pod in ${DEBUG_PODS[@]}; do
     log "Deleting pod $pod"
-    oc delete -n $DNS_PCAP_NS pod $pod
+    oc delete -n $DNS_PCAP_NS pod $pod &
   done
+  wait
 }
 
-tcpdump_node() {
+start_debug_node() {
   local node=$1; shift
   local node_name=$(basename $node)
 
-  log Starting a debug pod for node: $node_name
+  log "Starting a debug pod for node: $node_name"
 
 
   local pod_manifest=$( oc debug $node --to-namespace="$DNS_PCAP_NS" -o json -- sleep infinity )
@@ -90,27 +105,52 @@ tcpdump_node() {
 
   oc wait --for=condition=Ready -n $DNS_PCAP_NS "pod/$pod_name"
 
+  return 0
+}
+
+tcpdump_node() {
+  local node_name=$1; shift
+  local pod_name=$1; shift
+
   local debug_script
 
   debug_script=$(cat <<-EOF
-pod_id=\$(chroot /host crictl ps -o json | jq -r '.containers[] | select(.labels["io.kubernetes.container.name"] == "dns") | .id')
-pod_pid=\$(chroot /host crictl inspect --output json \$pod_id | jq '.info.pid')
-nsenter -n -t \$pod_pid -- tcpdump -i any -nn -w /tmp/$node_name.pcap
+    pod_id=\$(chroot /host crictl ps -o json | jq -r '.containers[] | select(.labels["io.kubernetes.container.name"] == "dns") | .id')
+    pod_pid=\$(chroot /host crictl inspect --output json \$pod_id | jq '.info.pid')
+    nsenter -n -t \$pod_pid -- tcpdump -i any -nn -w /tmp/$node_name.pcap
 EOF
 )
+
+  log "starting tcpdump on pod: $pod_name"
 
   oc exec -t -n $DNS_PCAP_NS $pod_name  -- bash -c "$debug_script" &
   TCPDUMP_PIDS+=( $! )
   return 0
 }
 
-start_tcpdump() {
+start_tcpdump_all_nodes() {
+
+  echo -e \n
+  log "Starting tcpdump on all debug pods ... "
+  echo "-----------------------------------------------------"
+
+  local idx=0
+  for pod in ${DEBUG_PODS[@]}; do
+    local node=${NODE_NAMES[$idx]}
+
+    tcpdump_node $node $pod
+    (( idx++ )) || true   ### something is wrong; why does this error ?
+
+  done
+}
+
+start_debug_pods() {
+  sub_header "Starting Debug Pods on all nodes ... "
 
   local nodes=($(oc get nodes -o name))
   for n in ${nodes[@]}; do
-    tcpdump_node "$n"
+    start_debug_node "$n"
   done
-  #tcpdump_node "${nodes[1]}"
 
 }
 
@@ -123,7 +163,9 @@ main() {
   echo ==========================================================
 
   oc get ns $DNS_PCAP_NS -o name >/dev/null || oc create ns $DNS_PCAP_NS
-  start_tcpdump
+
+  start_debug_pods
+  start_tcpdump_all_nodes
 
   sleep 3
   echo ==========================================================
